bf00f3dc52f011cd43cada32958dc29ce43f1366
thampaponn <64070046@it.kmitl.ac.th>
2023-04-08 17:44:07 +0700

    manage-course layout

--- client/components/CardItem.vue
+++ client/components/CardItem.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+    <div class="max-w-sm bg-white border border-gray-200 rounded-3xl shadow">
         <a href="#">
-            <img class="rounded-t-lg h-[200px] w-[300px]"
+            <img class="rounded-t-lg h-[250px] w-[350px]"
                 src="https://scontent.fbkk8-2.fna.fbcdn.net/v/t1.15752-9/337518384_536926975283149_4316394894937360078_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=ae9488&_nc_ohc=RYCoqaICfZgAX9ZFWVU&_nc_ht=scontent.fbkk8-2.fna&oh=03_AdSM33hjyOJESvLeYCpPZkQF5tKW-j7FMq0pzpEz7eJD5A&oe=645839C9"
                 alt="" />
         </a>
@@ -10,20 +10,21 @@
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology
                     acquisitions 2021</h5>
             </a>
+            <hr class="h-[2px] my-4 bg-gray-800">
             <p class="mb-3 font-normal text-lg text-gray-700">ศาสตราจารย์ ธนกร ศรีวรรณวิทย์</p>
             <p class="mb-3 font-normal text-sm text-gray-700">ผู้เชี่ยวชาญด้านประดิษฐ์ระเบิดขวด, ได้รับดีกรีจากสถาบัน
                 Hogwarts
                 ในด้านเวทมนต์ศาสตร์ 2077</p>
-            <a href="#"
-                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-                Read more
-                <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd"
-                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                        clip-rule="evenodd"></path>
-                </svg>
-            </a>
+            <div class="flex justify-between mx-4">
+                <a href="#"
+                    class="inline-flex items-center justify-center w-28 px-3 py-2 text-normal font-normal text-center text-green-600 bg-transparent hover:text-green-800">
+                    แก้ไขคอร์ส
+                </a>
+                <a href="#"
+                    class="inline-flex items-center justify-center w-28 px-3 py-2 text-normal font-normal text-center text-red-700 bg-transparent hover:text-red-800">
+                    ลบคอร์ส
+                </a>
+            </div>
         </div>
     </div>
 </template>
--- client/pages/ManageCourse.vue
+++ client/pages/ManageCourse.vue
@@ -6,36 +6,38 @@
             <form class="w-7/12">
                 <label for="default-search"
                     class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                <div class="relative">
+                <div class="relative mx-4">
                     <input id="default-search" type="search"
-                        class="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-3xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+                        class="block w-full p-4 text-sm text-gray-900 border border-gray-950 rounded-3xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
                         placeholder="Search Mockups, Logos..." required>
                     <button type="submit"
-                        class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-3xl text-sm px-4 py-2">Search</button>
+                        class="text-white absolute right-2.5 bottom-2.5 bg-[#2B26D8] hover:bg-[#130ec7] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-3xl text-sm px-4 py-2">Search</button>
                 </div>
             </form>
-            <label for="" class="mx-4">จัดเรียง</label>
+            <label for="" class="mx-4">เรียงตาม</label>
             <div class="flex mx-4">
                 <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown"
-                    class="text-black border border-gray-950 bg-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-3xl text-sm w-48 px-4 py-2.5 text-center inline-flex justify-center items-center"
-                    type="button">เรียงตาม <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor"
+                    class="text-black border border-gray-950 bg-white hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-3xl text-sm w-48 px-4 py-2.5 text-center inline-flex justify-center items-center"
+                    type="button">
+                    โปรดเลือก <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor"
                         viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-                    </svg></button>
+                    </svg>
+                </button>
                 <!-- Dropdown menu -->
                 <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-48">
                     <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+                        <li>
+                            <a href="#" class="block px-4 py-2 hover:bg-gray-100">A - Z</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block px-4 py-2 hover:bg-gray-100">Z - A</a>
+                        </li>
                         <li>
                             <a href="#" class="block px-4 py-2 hover:bg-gray-100">วันที่ลงเรียน : (ก่อน - หลัง)</a>
                         </li>
                         <li>
                             <a href="#" class="block px-4 py-2 hover:bg-gray-100">วันที่ลงเรียน : (หลัง - ก่อน)</a>
-                        </li>
-                        <li>
-                            <a href="#" class="block px-4 py-2 hover:bg-gray-100">วันที่สร้าง(ก่อน - หลัง)</a>
-                        </li>
-                        <li>
-                            <a href="#" class="block px-4 py-2 hover:bg-gray-100">วันที่สร้าง(ก่อน - หลัง)</a>
                         </li>
                     </ul>
                 </div>
@@ -44,12 +46,13 @@
                 class="text-white bg-[#2B26D8] hover:bg-[#130ec7] focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 focus:outline-none">เพิ่มคอร์ส</button>
 
         </div>
-        <hr class="h-[2px] my-8 bg-gray-200">
+        <hr class="h-[2px] my-8 bg-gray-800">
         <div class="grid grid-cols-5 gap-10 mx-8 my-8">
             <div v-for="value, index in 10" :key="index">
                 <CardItem></CardItem>
             </div>
         </div>
+        <Footer></Footer>
     </div>
 </template>
   
