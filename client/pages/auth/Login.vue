55d4c560e3004ee122891bf96afc54b0bf87cef1
thampaponn <64070046@it.kmitl.ac.th>
2023-04-10 12:48:27 +0700

    [update] login

--- client/pages/auth/Login.vue
+++ client/pages/auth/Login.vue
@@ -1,22 +1,22 @@
 <template>
-    <div class="flex justify-center items-center">
-        <div class="flex justify-center items-center">
-            <div class="flex flex-col">
-                <h1>Login</h1>
+    <div class="flex justify-center items-center align-center">
+            <div class="flex flex-col space-y-6 w-[15%]">
+                <h1 class="text-4xl">Login</h1>
                 <div>
                     <p class="text-sm">email</p>
-                    <input class="border rounded-md" type="" name="" value="" placeholder="example@gmail.com">
+                    <input class="border rounded-md py-1 px-1 w-full" type="" name="" value="" placeholder="example@gmail.com">
                 </div>
                 <div>
-                    <p class="text-sm">email</p>
-                    <input class="border rounded-md" type="" name="" value="" placeholder="example@gmail.com">
+                    <p class="text-sm">password</p>
+                    <input class="border rounded-md py-1 px-1 w-full" type="" name="" value="" placeholder="includes 0-9,  abc,  !@$">
+                    <p class="text-sm">forget password?</p>
                 </div>
-                <p class="text-sm">forget password?</p>
                 <button type="" class="bg-[#2B26D8] text-white rounded-full py-1">login</button>
-                <p></p>
+                <div class="flex justify-between text-sm">
+                    <p>not member?</p>
+                    <p class="text-[#AAAAAA]">click to register</p>
+                </div>
             </div>
-        </div>
-    </div>
     </div>
 </template>
 <script>
