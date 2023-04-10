5c21d940c0c979c0b6df163a5062ab4d3050f551
64070040Thanakorn <88426940+64070040Thanakorn@users.noreply.github.com>
2023-04-10 11:28:20 +0700

    update

---
+++ client/pages/search.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="bg-[#F5F4F8] py-20 px-72 space-y-10 rounded-br-[300px]">
+      <div class="flex justify-between">
+        <div class="space-y-4 justify-center flex flex-col">
+          <p class="font-medium text-6xl">หาคอร์สเรียนที่เหมาะกับคุณ</p>
+          <p class="text-4xl text-[#2B26D8]">เว็บไซด์จัดหาคอร์สเรียนที่ดีที่สุด</p>
+          <p class="text-[#9F9F9F]">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has <br />been the industry's standard dummy text ever since the
+            1500s
+          </p>
+        </div>
+        <div>
+          <img class="w-full h-full" src="../assets/search/search_01.png" alt="" />
+        </div>
+      </div>
+      <i class="fa-solid fa-magnifying-glass"></i>
+      <div class="relative flex items-center">
+      
+        <input
+          class="flex w-full px-6 py-3 rounded-[30px] absolute"
+          type=""
+          name=""
+          value=""
+          placeholder="ค้นหาเรื่องที่คุณสนใจ..."
+        />
+        <button class="absolute bg-[#2B26D8] px-10 py-1 rounded-[15px] text-white right-0 mr-6">ค้นหา</button>
+      </div>
+    </div>
+    <div></div>
+  </div>
+</template>
