5a2fe007b7bbafda58d558c8af80fd54f9663269
thampaponn <64070046@it.kmitl.ac.th>
2023-04-10 12:40:50 +0700

    [adding] login page

---
+++ client/pages/auth/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="flex justify-center items-center">
+        <div class="flex justify-center items-center">
+            <div class="flex flex-col">
+                <h1>Login</h1>
+                <div>
+                    <p class="text-sm">email</p>
+                    <input class="border rounded-md" type="" name="" value="" placeholder="example@gmail.com">
+                </div>
+                <div>
+                    <p class="text-sm">email</p>
+                    <input class="border rounded-md" type="" name="" value="" placeholder="example@gmail.com">
+                </div>
+                <p class="text-sm">forget password?</p>
+                <button type="" class="bg-[#2B26D8] text-white rounded-full py-1">login</button>
+                <p></p>
+            </div>
+        </div>
+    </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'LoginView'
+}
+</script>
