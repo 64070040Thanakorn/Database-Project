97df87f7a02e7f0110deb2e8a23c922848d7c8ab
Thanakorn Sriwannawit <64070040@kmitl.ac.th>
2023-04-16 16:43:59 +0700

    Update db.sql

--- server/db.sql
+++ server/db.sql
@@ -1,125 +1,125 @@
--- -- CreateTable
--- CREATE TABLE `Users` (
---     `user_id` VARCHAR(191) NOT NULL,
---     `first_name` VARCHAR(191) NOT NULL,
---     `last_name` VARCHAR(191) NOT NULL,
---     `username` VARCHAR(191) NOT NULL,
---     `password` VARCHAR(191) NOT NULL,
---     `email` VARCHAR(191) NOT NULL,
---     `image` VARCHAR(191) NULL,
---     `role` ENUM('Student', 'Professor', 'Admin') NOT NULL DEFAULT 'Student',
+-- CreateTable
+CREATE TABLE `Users` (
+    `user_id` VARCHAR(191) NOT NULL,
+    `first_name` VARCHAR(191) NOT NULL,
+    `last_name` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `image` VARCHAR(191) NULL,
+    `role` ENUM('Student', 'Professor', 'Admin') NOT NULL DEFAULT 'Student',
 
---     UNIQUE INDEX `Users_email_key`(`email`),
---     PRIMARY KEY (`user_id`)
--- ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    UNIQUE INDEX `Users_email_key`(`email`),
+    PRIMARY KEY (`user_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- -- CreateTable
--- CREATE TABLE `Students` (
---     `student_id` VARCHAR(191) NOT NULL,
---     `user_id` VARCHAR(191) NOT NULL,
---     `job` VARCHAR(191) NULL,
+-- CreateTable
+CREATE TABLE `Students` (
+    `student_id` VARCHAR(191) NOT NULL,
+    `user_id` VARCHAR(191) NOT NULL,
+    `job` VARCHAR(191) NULL,
 
---     UNIQUE INDEX `Students_user_id_key`(`user_id`),
---     PRIMARY KEY (`student_id`)
--- ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    UNIQUE INDEX `Students_user_id_key`(`user_id`),
+    PRIMARY KEY (`student_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- -- CreateTable
--- CREATE TABLE `Professors` (
---     `professor_id` VARCHAR(191) NOT NULL,
---     `user_id` VARCHAR(191) NOT NULL,
---     `info` VARCHAR(191) NULL,
+-- CreateTable
+CREATE TABLE `Professors` (
+    `professor_id` VARCHAR(191) NOT NULL,
+    `user_id` VARCHAR(191) NOT NULL,
+    `info` VARCHAR(191) NULL,
 
---     UNIQUE INDEX `Professors_user_id_key`(`user_id`),
---     PRIMARY KEY (`professor_id`)
--- ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    UNIQUE INDEX `Professors_user_id_key`(`user_id`),
+    PRIMARY KEY (`professor_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- -- CreateTable
--- CREATE TABLE `Course` (
---     `course_id` VARCHAR(191) NOT NULL,
---     `title` VARCHAR(191) NOT NULL,
---     `description` VARCHAR(255) NULL,
---     `price` INTEGER NOT NULL,
---     `level` VARCHAR(191) NOT NULL,
---     `received` VARCHAR(191) NOT NULL,
---     `create_date` DATETIME(3) NOT NULL,
---     `end_date` DATETIME(3) NOT NULL,
---     `professor_id` VARCHAR(191) NOT NULL,
---     `start_date` DATETIME(3) NOT NULL,
---     `status` BOOLEAN NOT NULL,
---     `thumbnail` VARCHAR(191) NULL,
+-- CreateTable
+CREATE TABLE `Course` (
+    `course_id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(255) NULL,
+    `price` INTEGER NOT NULL,
+    `level` VARCHAR(191) NOT NULL,
+    `received` VARCHAR(191) NOT NULL,
+    `create_date` DATETIME(3) NOT NULL,
+    `end_date` DATETIME(3) NOT NULL,
+    `professor_id` VARCHAR(191) NOT NULL,
+    `start_date` DATETIME(3) NOT NULL,
+    `status` BOOLEAN NOT NULL,
+    `thumbnail` VARCHAR(191) NULL,
 
---     PRIMARY KEY (`course_id`)
--- ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    PRIMARY KEY (`course_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- -- CreateTable
--- CREATE TABLE `Comments` (
---     `comment_id` VARCHAR(191) NOT NULL,
---     `content` VARCHAR(191) NOT NULL,
---     `user_id` VARCHAR(191) NOT NULL,
---     `course_id` VARCHAR(191) NOT NULL,
+-- CreateTable
+CREATE TABLE `Comments` (
+    `comment_id` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+    `user_id` VARCHAR(191) NOT NULL,
+    `course_id` VARCHAR(191) NOT NULL,
 
---     PRIMARY KEY (`comment_id`)
--- ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    PRIMARY KEY (`comment_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- -- CreateTable
--- CREATE TABLE `CourseRating` (
---     `course_rating_id` VARCHAR(191) NOT NULL,
---     `course_id` VARCHAR(191) NOT NULL,
---     `student_id` VARCHAR(191) NOT NULL,
+-- CreateTable
+CREATE TABLE `CourseRating` (
+    `course_rating_id` VARCHAR(191) NOT NULL,
+    `course_id` VARCHAR(191) NOT NULL,
+    `student_id` VARCHAR(191) NOT NULL,
 
---     PRIMARY KEY (`course_rating_id`)
--- ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    PRIMARY KEY (`course_rating_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- -- CreateTable
--- CREATE TABLE `ProfessorRating` (
---     `professor_rating_id` VARCHAR(191) NOT NULL,
---     `professor_id` VARCHAR(191) NOT NULL,
---     `student_id` VARCHAR(191) NOT NULL,
+-- CreateTable
+CREATE TABLE `ProfessorRating` (
+    `professor_rating_id` VARCHAR(191) NOT NULL,
+    `professor_id` VARCHAR(191) NOT NULL,
+    `student_id` VARCHAR(191) NOT NULL,
 
---     PRIMARY KEY (`professor_rating_id`)
--- ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    PRIMARY KEY (`professor_rating_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- -- CreateTable
--- CREATE TABLE `StudentsEnroll` (
---     `student_id` VARCHAR(191) NOT NULL,
---     `course_id` VARCHAR(191) NOT NULL,
---     `enroll_date` DATETIME(3) NOT NULL,
+-- CreateTable
+CREATE TABLE `StudentsEnroll` (
+    `student_id` VARCHAR(191) NOT NULL,
+    `course_id` VARCHAR(191) NOT NULL,
+    `enroll_date` DATETIME(3) NOT NULL,
 
---     PRIMARY KEY (`student_id`, `course_id`)
--- ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    PRIMARY KEY (`student_id`, `course_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- -- AddForeignKey
--- ALTER TABLE `Students` ADD CONSTRAINT `Students_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `Users`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+-- AddForeignKey
+ALTER TABLE `Students` ADD CONSTRAINT `Students_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `Users`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
--- -- AddForeignKey
--- ALTER TABLE `Professors` ADD CONSTRAINT `Professors_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `Users`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+-- AddForeignKey
+ALTER TABLE `Professors` ADD CONSTRAINT `Professors_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `Users`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
--- -- AddForeignKey
--- ALTER TABLE `Course` ADD CONSTRAINT `Course_professor_id_fkey` FOREIGN KEY (`professor_id`) REFERENCES `Professors`(`professor_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+-- AddForeignKey
+ALTER TABLE `Course` ADD CONSTRAINT `Course_professor_id_fkey` FOREIGN KEY (`professor_id`) REFERENCES `Professors`(`professor_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
--- -- AddForeignKey
--- ALTER TABLE `Comments` ADD CONSTRAINT `Comments_course_id_fkey` FOREIGN KEY (`course_id`) REFERENCES `Course`(`course_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+-- AddForeignKey
+ALTER TABLE `Comments` ADD CONSTRAINT `Comments_course_id_fkey` FOREIGN KEY (`course_id`) REFERENCES `Course`(`course_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
--- -- AddForeignKey
--- ALTER TABLE `Comments` ADD CONSTRAINT `Comments_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `Users`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+-- AddForeignKey
+ALTER TABLE `Comments` ADD CONSTRAINT `Comments_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `Users`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
--- -- AddForeignKey
--- ALTER TABLE `CourseRating` ADD CONSTRAINT `CourseRating_course_id_fkey` FOREIGN KEY (`course_id`) REFERENCES `Course`(`course_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+-- AddForeignKey
+ALTER TABLE `CourseRating` ADD CONSTRAINT `CourseRating_course_id_fkey` FOREIGN KEY (`course_id`) REFERENCES `Course`(`course_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
--- -- AddForeignKey
--- ALTER TABLE `CourseRating` ADD CONSTRAINT `CourseRating_student_id_fkey` FOREIGN KEY (`student_id`) REFERENCES `Students`(`student_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+-- AddForeignKey
+ALTER TABLE `CourseRating` ADD CONSTRAINT `CourseRating_student_id_fkey` FOREIGN KEY (`student_id`) REFERENCES `Students`(`student_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
--- -- AddForeignKey
--- ALTER TABLE `ProfessorRating` ADD CONSTRAINT `ProfessorRating_professor_id_fkey` FOREIGN KEY (`professor_id`) REFERENCES `Professors`(`professor_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+-- AddForeignKey
+ALTER TABLE `ProfessorRating` ADD CONSTRAINT `ProfessorRating_professor_id_fkey` FOREIGN KEY (`professor_id`) REFERENCES `Professors`(`professor_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
--- -- AddForeignKey
--- ALTER TABLE `ProfessorRating` ADD CONSTRAINT `ProfessorRating_student_id_fkey` FOREIGN KEY (`student_id`) REFERENCES `Students`(`student_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+-- AddForeignKey
+ALTER TABLE `ProfessorRating` ADD CONSTRAINT `ProfessorRating_student_id_fkey` FOREIGN KEY (`student_id`) REFERENCES `Students`(`student_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
--- -- AddForeignKey
--- ALTER TABLE `StudentsEnroll` ADD CONSTRAINT `StudentsEnroll_course_id_fkey` FOREIGN KEY (`course_id`) REFERENCES `Course`(`course_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+-- AddForeignKey
+ALTER TABLE `StudentsEnroll` ADD CONSTRAINT `StudentsEnroll_course_id_fkey` FOREIGN KEY (`course_id`) REFERENCES `Course`(`course_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
--- -- AddForeignKey
--- ALTER TABLE `StudentsEnroll` ADD CONSTRAINT `StudentsEnroll_student_id_fkey` FOREIGN KEY (`student_id`) REFERENCES `Students`(`student_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+-- AddForeignKey
+ALTER TABLE `StudentsEnroll` ADD CONSTRAINT `StudentsEnroll_student_id_fkey` FOREIGN KEY (`student_id`) REFERENCES `Students`(`student_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 
 -- DATA
