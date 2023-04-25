e1ece65f8239d732553ae3ffa1f2b01f75426b2d
pp3times <64070001@kmitl.ac.th>
2023-04-25 22:24:27 +0700

    fewza update

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
-
+app.use('/images', express.static('static/uploads'));
 app.use("/api",
  router.use("/user", UersRoute),
  router.use("/course", course),
